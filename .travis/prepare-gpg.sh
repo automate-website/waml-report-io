9e1e7342aefd8a5f958f876fa25aff9039d16290
Michael Lewkowski <michael@lewkowski.de>
2016-03-18 23:17:23 +0100

    added encrypted environment variables

--- .travis/prepare-gpg.sh
+++ .travis/prepare-gpg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if ( [ "$TRAVIS_BRANCH" = 'master' ] || [ ! -z "$TRAVIS_TAG" ] ) && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-	openssl aes-256-cbc -K $encrypted_172a703c35e0_key -iv $encrypted_172a703c35e0_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+	openssl aes-256-cbc -K $encrypted_6f4d966a7212_key -iv $encrypted_6f4d966a7212_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
 	gpg --fast-import .travis/codesigning.asc
 fi
